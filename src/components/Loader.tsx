1ce6e2438c0e596858686d76b47f14aad953d2b3
Mohammed <iraqiProfessionalDesigner@gmail.com>
2023-07-31 23:42:29 +0700

    first commit

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,23 +1,24 @@
 "use client";
 import React from 'react';
 export default function Loader() {
-    const array = Array.from({ length: 20 }, (e, i) => i);
+    const array = Array.from({ length: 50 }, (e, i) => i);
     return (
-        <>{array.map((item) => {
-            return (
-                <div className="col-md-5 col-lg-3 col-sm-11 mb-4" key={item}>
-                    <div className="card ">
-                        <div className="card-body ">
-                            <div className="row gap-3">
-                                <div className="bg-light p-3 rounded"></div>
-                                <div className="bg-light p-3 rounded"></div>
-                                <div className="bg-light p-3 rounded"></div>
+        <>
+            {array.map((item) => {
+                return (
+                    <div className="col-md-5 col-lg-3 col-sm-11 mb-4" key={item} >
+                        <div className="card " style={{ minHeight: "555px" }}>
+                            <div className="card-body ">
+                                <div className="d-flex row gap-3 m-0">
+                                    <div className="col-12 bg-light p-3 rounded" style={{ minHeight: "280px" }}></div>
+                                    <div className="col-12 bg-light p-3 rounded"></div>
+                                    <div className="col-12 bg-light p-3 rounded "></div>
+                                </div>
+                                <div className="bg-light p-4 rounded mt-5"></div>
                             </div>
-                            <div className="bg-light p-4 rounded mt-2"></div>
                         </div>
                     </div>
-                </div>)
-
-        })}
+                )
+            })}
         </>)
 }
